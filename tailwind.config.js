b9ce185161e6fdca83930627d0cd13b1c120e22e
Zinger1988 <sergeygonchar88@gmail.com>
2024-08-27 17:22:30 +0300

    DONE: feature/tailwindBaseConfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,88 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: "Nunito Sans, sans-serif",
+        heading: "Poppins, asns-serif",
+      },
+      colors: {
+        primary: {
+          100: "#FFF7D7",
+          200: "#FFF2BB",
+          300: "#FFE885",
+          400: "#FFE451",
+          500: "#E5C74A",
+          600: "#CCB244",
+          700: "#B29C3D",
+          800: "#998636",
+          900: "#7F702E",
+        },
+      },
+      lineHeight: {
+        default: 1.5,
+      },
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addComponents, theme }) {
+      const breakpoints = theme("screens");
+
+      const components = {
+        ".text-body": {
+          [`@media (max-width: ${breakpoints.lg})`]: {
+            fontSize: ".875rem",
+          },
+          [`@media (min-width: ${breakpoints.lg})`]: {
+            fontSize: "1rem",
+          },
+        },
+        ".text-body-sm": {
+          [`@media (max-width: ${breakpoints.lg})`]: {
+            fontSize: ".75rem",
+          },
+          [`@media (min-width: ${breakpoints.lg})`]: {
+            fontSize: ".875rem",
+          },
+        },
+        ".text-h1": {
+          [`@media (max-width: ${breakpoints.lg})`]: {
+            fontSize: "2.078rem",
+          },
+          [`@media (min-width: ${breakpoints.lg})`]: {
+            fontSize: "2.375rem",
+          },
+        },
+        ".text-h2": {
+          [`@media (max-width: ${breakpoints.lg})`]: {
+            fontSize: "1.53rem",
+          },
+          [`@media (min-width: ${breakpoints.lg})`]: {
+            fontSize: "1.75rem",
+          },
+        },
+        ".text-h3": {
+          [`@media (max-width: ${breakpoints.lg})`]: {
+            fontSize: "1.148rem",
+          },
+          [`@media (min-width: ${breakpoints.lg})`]: {
+            fontSize: "1.3125rem",
+          },
+        },
+        ".text-h4": {
+          [`@media (max-width: ${breakpoints.lg})`]: {
+            fontSize: "1rem",
+          },
+          [`@media (min-width: ${breakpoints.lg})`]: {
+            fontSize: "1.148rem",
+          },
+        },
+      };
+
+      addComponents(components);
+    },
+  ],
 };
